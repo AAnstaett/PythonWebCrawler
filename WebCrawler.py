ed787220117c3e8bad8b897f9395642dbe47ec39
Andrew Anstaett <andrew@aanstaett.com>
2015-04-06 22:14:22 -0500

    updated WebCrawler to meet new requirements

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,44 +1,51 @@
+#Andrew Anstaett
+#CNN Web Crawler in Python
+
 from bs4 import BeautifulSoup
 import urllib2
 
 
 
 def getSoup(url):
+	#requests html from website
 	html = urllib2.urlopen(url).read()
+	#makes it into a BeautifulSoup datatype that we can use to parse out the infomation needed
 	soup = BeautifulSoup(html)
+	#returns it to 'main'
 	return soup
 
-def pullCategories(soup,category):
-	for a in soup.find_all("a",text=category):
-		categories = a['href']
+def pullArticles(soup,category):
+	#for all the unordered lists
+	#the categories are seperated into diffrent unordered lists, so we seperate them
+	#and find the ones we are looking for
+	for ul in soup.find_all('ul'):
+		#h2 is the title of the category
+		for h2 in ul.children:
+			#we find the category we are looking for
+			if(h2.text == category):
+				#cnn uses the class 'cd__headline-text' for the headline articles titles
+				#we look for all the headline titles
+				for span in ul.select(".cd__headline-text"):
+					#we print the titles
+					print("\t" + span.text)
 
-	return categories
-
-def pullArticles(soup):
-	for span in soup.find_all("span",itemprop='name'):
-		print("\t" + span.string)
 
 
-	return
 
+#'main'
 cnn = getSoup("http://www.cnn.com")
-category = pullCategories(cnn,"Tech")
 
-print("Tech:")
-category = pullCategories(cnn,"Tech")
-articles = getSoup("http://www.cnn.com" + category)
-pullArticles(articles)
+print("Top Stories:")
+pullArticles(cnn,"Top Stories")
 
 print("\n\n")
 
-print("Entertainment:")
-category = pullCategories(cnn,"Entertainment")
-articles = getSoup("http://www.cnn.com" + category)
-pullArticles(articles)
+print("News and Buzz:")
+pullArticles(cnn,"News and Buzz")
 
 print("\n\n")
 
-print("Travel:")
-category = pullCategories(cnn,"Travel")
-articles = getSoup("http://www.cnn.com" + category)
-pullArticles(articles)
+print("Sports:")
+pullArticles(cnn,"Sports")
+
+print("\n\n")
